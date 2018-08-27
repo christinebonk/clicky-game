1bf83b22a9aaa100df31039d12fd491a87870d23
Christine Bonk <christinebonk@gmail.com>
2018-08-27 09:55:27 -0400

    made squares appear

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Game from "./components/game/game.js";
+import Game from "./components/game/";
 
 
 
--- myapp/src/components/game/game.js
+++ myapp/src/components/game/game.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Board from "./components/game";
-import Square from "./components/game";
+import Board from "./board";
 
 class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      squares: [1,2,3,4,5,6,7,8,9,10,11,12],
       highScore: 0,
       stepNumber: 0
           };
@@ -17,7 +17,7 @@ class Game extends React.Component {
       <div className="game">
         <div className="game-board">
           <Board
-            
+            squares = {this.state.squares}
             onClick={i => this.handleClick(i)}
           />
         </div>
